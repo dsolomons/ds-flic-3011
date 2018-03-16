1c8f711fa42adff978145e026488625bb11dfd42
GreenSol <dsolomons@salesforce.com>
2018-03-15 18:21:33 -0700

    .
    .

--- web/green.php
+++ web/green.php
@@ -102,7 +102,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n  \"SubscriberKeys\": [\n    \"0036A00000Pe4nYQAR\"\n  ],\n  \"Override\": true,\n  \"MessageText\": \"$message\",\n  \"title\": \"title\",\n  \"subtitle\": \"$subtitle\",\n  \"Badge\": \"+1\"\n}",
+  CURLOPT_POSTFIELDS => "{\n  \"SubscriberKeys\": [\n    \"0036A00000PVrDSQA1\"\n  ],\n  \"Override\": true,\n  \"MessageText\": \"$message\",\n  \"title\": \"title\",\n  \"subtitle\": \"$subtitle\",\n  \"Badge\": \"+1\"\n}",
   CURLOPT_HTTPHEADER => array(
     "authorization: Bearer $accessToken",
     "cache-control: no-cache",
